cf1e6bf4ec09a413d99426ffc140d97325787b42
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2024-03-18 17:10:49 -0300

    First commit

---
+++ tests/units/services/user-service.test.js
@@ -0,0 +1,60 @@
+const { faker } = require('@faker-js/faker')
+const UserService = require('../../../src/services/user-service')
+const User = require('../../../src/schemas/User')
+
+const UserMock = {
+    findOne: async () => null, // Essa função do Mongoose retorna null quando não encontra o que foi pesquisado.
+    create: async () => ({ id: faker.database.mongodbObjectId()}),
+    userCredentials: async () => ({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+    })
+}
+
+const user = {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+}
+
+
+describe('User service test', () => {
+    test('Shoud return an ID when a new user is created', async () => {
+        jest.spyOn(User, 'create').mockImplementationOnce(UserMock.create)
+        
+        console.log(user)
+
+        const userCreated = await UserService.createUser(user)
+
+        expect(userCreated).toHaveProperty('id')
+    })
+
+    test('Shoud return true if user exists and the passwords match.', async () => {
+        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.userCredentials)
+
+        const userExist = await UserService.userExistsAndCheckPassword(UserMock.userCredentials)
+
+        expect(userExist).toBe(true)
+
+    })
+
+    test('Should return false if an user do not exist.', async () => {
+        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.findOne)
+    
+        const userAbsent = await UserService.userExistsAndCheckPassword(UserMock.userCredentials)
+
+        expect(userAbsent).toBe(false)
+    })  
+
+    test('Should throws if password do not match', async () => {
+        jest.spyOn(User, 'findOne').mockImplementationOnce(UserMock.userCredentials)
+
+        try {
+            await UserService.userExistsAndCheckPassword(UserMock.userCredentials)
+        } catch (error) {
+            expect(error).toHaveProperty('status', 400)
+            expect(error).toHaveProperty('message', 'As senhas não batem')
+        }
+    })
+
+})
